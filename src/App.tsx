a2d1278e5034f5a0eec165539f2a836f0d8f4ee8
unknown <jhonatasenac15@gmail.com>
2024-08-26 12:36:20 -0300

    2° commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import './App.css'
-import Contador from './pages/contador/contador'
+import { useState } from 'react'
 import Home from './pages/home/Home'
+import Contador from './pages/contador/Contador'
 import Tarefa from './pages/tarefa/Tarefa'
 
 function App() {
-
+// const [nome_estado,nome_funcao_estado] = useState(valor_inicial_estado) -> Criando um estado em React
   return (
     <>
-        <Home 
-          titulo = "Minha primeira postagem"
-          texto = "Texto da minha primeira postagem"
+        {/*<Home
+          titulo="Título da minha primeira postagem"
+          texto="Texto da minha primeira postagem"
         />
-
-         <Contador />
-
-        <Tarefa />
+        <Home
+          titulo="Título da minha segunda postagem"
+          texto="Texto da minha segunda postagem"
+        /> */}
+        {/*
+          <Contador/>
+        */}
+        <Tarefa/>
     </>
   )
 }
